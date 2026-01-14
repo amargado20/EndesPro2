161efdbdc51d00e3d07343af90926f79ff33b49c
Juan Francisco Navarro Pegalajar <jnavpeg2006@g.educaand.es>
2026-01-14 13:27:42 +0100

    Version2.0

--- src/Hola.java
+++ src/Hola.java
@@ -1,5 +1,7 @@
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Hola");
+        System.out.println("Hola");
     }
 }
